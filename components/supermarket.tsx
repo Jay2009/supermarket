274beb279c9ccf75ca9c19f54af7e27ec8782fd9
Jaemin Kim <jaem2009@naver.com>
2022-06-24 18:41:35 +0900

    supermarket_v2

--- components/supermarket.tsx
+++ components/supermarket.tsx
@@ -3,7 +3,7 @@ import SuperMarketTemplate from "../components/supermarketTemplate"
 import ShopItemList from "./shopItemList"
 
 const SuperMarket = () => {
-  return <SuperMarketTemplate items={<ShopItemList />} basket={null} />
+  return <SuperMarketTemplate items={null} basket={null} />
 }
 
 export default SuperMarket
